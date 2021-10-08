93b2f1d6d3026215539ec1947048139b8d9f30a1
Missy Maloney <missymaloney1@gmail.com>
2021-10-08 18:44:24 -0400

    removed logs statement

--- src/models/index.js
+++ src/models/index.js
@@ -9,7 +9,6 @@ if (process.env.DATABASE_URL) {
   sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     protocol: 'postgres',
-    logging: true,
   });
 } else {
   sequelize = new Sequelize(
